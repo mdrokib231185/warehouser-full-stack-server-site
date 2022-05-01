799ac2079efe63dceb4cee1910f2b1e4901efd5b
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-01 11:44:34 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+// middleWare
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixvks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+      await client.connect()
+      try {
+            const productCollection = client.db("assignment-11").collection("products") 
+            app.get('/products', async (req, res) => {
+                  const query = {}
+                  const cursor = productCollection.find(query)
+                  const services = await cursor.toArray()  
+                  res.send(services)
+                  
+            })  
+      }
+      finally {
+            
+      }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+      res.send('Running Assignment 11')
+})
+
+app.listen(port, () => {
+      console.log('lissting port', port)
+})
